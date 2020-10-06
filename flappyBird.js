540cd1471584f488d5af35994ec962fa94f560ad
your name here <werock2000@gmail.com>
2020-10-05 23:53:13 -0700

    Modify pics

--- flappyBird.js
+++ flappyBird.js
@@ -34,7 +34,7 @@ var fly = new Audio();
 var scor = new Audio();  
 
 fly.src = "sounds/fly.mp3";
-scor.src = "sounds/score.mp3";
+scor.src = "sounds/score.mp3";  
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
