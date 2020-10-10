76aac97632b8ac445618b310943a860c13827031
your name here <werock2000@gmail.com>
2020-10-10 01:22:13 -0700

    Modified the shark pic

--- flappyBird.js
+++ flappyBird.js
@@ -5,7 +5,7 @@ var ctx = cvs.getContext("2d");
 
 var bird = new Image();
 var bg = new Image();  
-var fg = new Image();
+var fg = new Image();  
 var pipeNorth = new Image();
 var pipeSouth = new Image();  
       
