fa5d8bcd43029116297c09ac92f22f3e7e3fa350
your name here <werock2000@gmail.com>
2020-10-17 00:55:51 -0700

    Modified the shark pic

--- flappyBird.js
+++ flappyBird.js
@@ -107,7 +107,7 @@ function draw(){
         
        
      
-        if( pipe[i].x == 125 ){  
+        if( pipe[i].x == 125 ){   
             pipe.push({
                 x : cvs.width,
                 y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
