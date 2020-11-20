b39639632d49b38e889a581720ef0c204b9f9b19
your name here <werock2000@gmail.com>
2020-11-19 23:55:59 -0800

    Add buttons

--- flappyFish.js
+++ flappyFish.js
@@ -121,7 +121,7 @@ function initiate_the_game(){
     // ctx.drawImage(bg,0,0);
     if (bg.complete) {
         ctx.drawImage(bg, 0, 0);
-    } else {
+    } else { 
         bg.onload = function () { 
             ctx.drawImage(bg, 0, 0);     
         };
