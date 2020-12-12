5775b93c79970386a46d3a3b0df21a71745af54a
st <werock2000@gmail.com>
2020-12-12 01:21:12 -0800

    Modify pics

--- flappyFish.js
+++ flappyFish.js
@@ -182,7 +182,7 @@ function draw(hardstart){
            // show score
         //   if (score<5) alert(`Your score is ${score}. Practise makes perfect. Better luck next time!`)
         //   if (score>= 5) alert(`Your score is ${score}.Good job! Most people get less than 9 points!`)
-  
+   
   
            bX = 10;
            bY = 150;
