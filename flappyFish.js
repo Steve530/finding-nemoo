1725d6a7139c7f2f61fc10c3615d3c0c48045509
st <werock2000@gmail.com>
2020-12-28 22:12:26 -0800

    Modify game UX

--- flappyFish.js
+++ flappyFish.js
@@ -178,8 +178,7 @@ function draw(hardstart){
         var game_over = false 
         if (is_dead)
                  {
-           // location.reload(); // reload the page
-           // show score
+         // show score
         //   if (score<5) alert(`Your score is ${score}. Practise makes perfect. Better luck next time!`)
         //   if (score>= 5) alert(`Your score is ${score}.Good job! Most people get less than 9 points!`)
    
