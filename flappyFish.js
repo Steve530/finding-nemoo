b9933fc82cc22e76d6ff8e84c7665e96fa94ce79
your name here <werock2000@gmail.com>
2021-02-21 23:34:02 -0800

    add new pics

--- flappyFish.js
+++ flappyFish.js
@@ -182,10 +182,10 @@ function draw(hardstart){
         //   if (score<5) alert(`Your score is ${score}. Practise makes perfect. Better luck next time!`)
         //   if (score>= 5) alert(`Your score is ${score}.Good job! Most people get less than 9 points!`)
            
-                    
+                     
            bX = 10;           
            bY = 150;                 
-           pipe = [];                          
+           pipe = [];                           
                        
             pipe[0] = {          
                 x : cvs.width,                   
