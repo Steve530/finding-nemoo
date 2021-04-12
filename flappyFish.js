2a6b7614c9b6fc2b462fb1a9da394bfde724eb26
your name here <werock2000@gmail.com>
2021-04-12 16:00:19 -0700

    add news

--- flappyFish.js
+++ flappyFish.js
@@ -53,7 +53,7 @@ var fly = new Audio();
 var scor = new Audio();  
  
 fly.src = "sounds/fly.mp3"; 
-scor.src = "sounds/score.mp3";    
+scor.src = "sounds/score.mp3";     
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
