7da0a913fdf7554835967d9f1d115db137cfe81d
your name here <werock2000@gmail.com>
2021-04-15 06:17:42 -0700

    add news

--- flappyFish.js
+++ flappyFish.js
@@ -50,7 +50,7 @@ var score = 0;
 // audio files         
               
 var fly = new Audio();     
-var scor = new Audio();    
+var scor = new Audio();     
   
 fly.src = "sounds/fly.mp3";  
 scor.src = "sounds/score.mp3";     
